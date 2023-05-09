a9ffb2f1a696e89aacc3e49654046691f7cb1e36
biplob14 <biploburrahaman@gmail.com>
2023-05-09 18:20:57 +0600

    feat
    postgres connection and listing item for single page

--- product_details/main.py
+++ product_details/main.py
@@ -1,4 +1,4 @@
-from .additionals import prepareQuery
+from package.additionals import prepareQuery
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 query_str = "iphone x"
 
-url = prepareQuery(query_str="mobile phone")
+url = prepareQuery(query_str)
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
--- product_details/package/additionals.py
+++ product_details/package/additionals.py
@@ -1,6 +1,6 @@
 def prepareQuery(query_str):
     ''' process search string for url '''
     template = "https://www.amazon.com/s?k={}&ref=nb_sb_noss_2"
-    search_term = search_term.replace(' ', '+')
+    search_term = query_str.replace(' ', '+')
     
     return template.format(search_term)
