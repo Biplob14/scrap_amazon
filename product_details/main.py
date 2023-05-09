b530765c4266611d83a89b14efff0bb457942701
biplob14 <biploburrahaman@gmail.com>
2023-05-09 18:21:02 +0600

    feat
    postgres connection and listing item for single page

--- product_details/main.py
+++ product_details/main.py
@@ -4,12 +4,36 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import psycopg2
 
+# database connection
+conn = psycopg2.connect(
+    database="amazon_prod", user='amazon', password='amazon', host='localhost', port= '5432'
+)
+curs = conn.cursor()
 query_str = "iphone x"
-
 url = prepareQuery(query_str)
 
+# variable declare
+product_name = []
+product_link = []
+
+# setup driver
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, )
 driver.get(url)
+
+# scrapping
+items = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@data-component-type, "s-search-result")]'))
+)
+for item in items:
+    product_link = item.find_element(By.XPATH, './/a[@class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]').get_attribute("href")
+    product_name = item.find_element(By.XPATH, './/a[@class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]//span')
+
+    prod_name = product_name.text.replace("'", "'\'")
+    curs.execute(f"insert into product_list(product_title, product_link) values('{prod_name}', '{product_link}')")
+    conn.commit()
+
+driver.quit()
